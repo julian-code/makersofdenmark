26aab6482c20f0f026b6c3e46497ddbd799375ef
mnabe <32497085+mnabe@users.noreply.github.com>
2020-09-30 07:34:57 +0200

    Added generic DbContext to our generic repository

--- src/MakersOfDenmark.Domain/Repositories/Repository.cs
+++ src/MakersOfDenmark.Domain/Repositories/Repository.cs
@@ -1,10 +1,37 @@
-﻿using System;
+﻿using MakersOfDenmark.Domain.Interfaces.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MakersOfDenmark.Domain.Repositories
 {
-    class Repository
+    class Repository<TEntity, TId> : IRepository<TEntity, TId>
     {
+        protected readonly DbContext Context;
+        public Repository(DbContext context)
+        {
+            Context = context;
+        }
+        public Task Create(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Read(TId id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

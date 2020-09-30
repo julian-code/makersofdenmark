50b9f149147827d959f241bcdcc668671afc8478
mnabe <32497085+mnabe@users.noreply.github.com>
2020-09-30 08:27:19 +0200

    Added class constraint to IRepository

--- src/MakersOfDenmark.Domain/Interfaces/Persistence/IRepository.cs
+++ src/MakersOfDenmark.Domain/Interfaces/Persistence/IRepository.cs
@@ -2,7 +2,7 @@ using System.Threading.Tasks;
 
 namespace MakersOfDenmark.Domain.Interfaces.Persistence
 {
-    public interface IRepository <TEntity, TId>
+    public interface IRepository <TEntity, TId> where TEntity : class
     {
         Task Create(TEntity entity);
         Task Read(TId id);

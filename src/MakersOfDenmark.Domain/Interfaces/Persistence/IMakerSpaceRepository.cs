36b2c25baf5424b41a90e8fe235317c7243bda76
mnabe <32497085+mnabe@users.noreply.github.com>
2020-09-30 06:50:35 +0200

    Added a few repository interface methods

--- src/MakersOfDenmark.Domain/Interfaces/Persistence/IMakerSpaceRepository.cs
+++ src/MakersOfDenmark.Domain/Interfaces/Persistence/IMakerSpaceRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using MakersOfDenmark.Domain.Models;
 using MakersOfDenmark.Shared;
 
@@ -7,6 +8,10 @@ namespace MakersOfDenmark.Domain.Interfaces.Persistence
 {
     public interface IMakerSpaceRepository : IRepository<MakerSpaceEntity, Guid>
     {
+        MakerSpace Add(MakerSpace makerSpace);
+        MakerSpace Remove(MakerSpaceId makerSpaceId)
         IEnumerable<MakerSpace> GetAll();
+        MakerSpace GetMakerSpaceById(MakerSpaceId makerSpaceId);
+
     }
 }

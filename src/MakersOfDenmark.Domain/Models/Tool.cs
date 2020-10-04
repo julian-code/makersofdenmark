1e5d6ee51879cfa2bdef6131405f109bb266661e
julian-code <54644662+julian-code@users.noreply.github.com>
2020-10-04 12:35:57 +0200

    Removed join table. Updated tool to int id instead of guid.

--- src/MakersOfDenmark.Domain/Models/Tool.cs
+++ src/MakersOfDenmark.Domain/Models/Tool.cs
@@ -5,7 +5,7 @@ namespace MakersOfDenmark.Domain.Models
 {
     public class Tool
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public ICollection<MakerSpace> MakerSpaces { get; set; }
         public ICollection<Category> Categories { get; set; }
         public string Make { get; set; }

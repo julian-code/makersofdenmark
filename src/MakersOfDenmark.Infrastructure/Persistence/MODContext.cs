00e6efc1ae245a38970d5671c2de736b5fa448c7
julian-code <54644662+julian-code@users.noreply.github.com>
2020-10-04 15:30:28 +0200

    Added infrastructure

---
+++ src/MakersOfDenmark.Infrastructure/Persistence/MODContext.cs
@@ -0,0 +1,14 @@
+﻿using MakersOfDenmark.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MakersOfDenmark.Infrastructure.Persistence
+{
+    public class MODContext : DbContext
+    {
+        public MODContext(DbContextOptions<MODContext> options): base(options)
+        {
+
+        }
+        public DbSet<MakerSpace> MakerSpace { get; set; }
+    }
+}

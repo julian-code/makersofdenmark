d8bf662db57d31b4bc5346cd60166b09289b7b5f
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-10-11 20:04:14 +0200

    MediatR registered

---
+++ src/MakersOfDenmark.Application/IServiceCollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿namespace MakersOfDenmark.Application
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using System.Reflection;
+    using MediatR;
+
+    public static class IServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServiceDependencies(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}

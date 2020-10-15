e4a8e25c517f21bad2612afde6b5ad980a7333c4
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-10-15 06:51:02 +0200

    added reference to infrastructure project

--- src/MakersOfDenmark.WebAPI/Startup.cs
+++ src/MakersOfDenmark.WebAPI/Startup.cs
@@ -1,4 +1,5 @@
 using MakersOfDenmark.Application;
+using MakersOfDenmark.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;

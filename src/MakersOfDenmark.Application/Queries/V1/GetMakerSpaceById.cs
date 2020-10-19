db05a16886d92acbe0c595e22b7ce200c145ccb0
julian-code <54644662+julian-code@users.noreply.github.com>
2020-10-19 19:54:56 +0200

    Removed unused usings.

--- src/MakersOfDenmark.Application/Queries/V1/GetMakerSpaceById.cs
+++ src/MakersOfDenmark.Application/Queries/V1/GetMakerSpaceById.cs
@@ -5,8 +5,6 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 

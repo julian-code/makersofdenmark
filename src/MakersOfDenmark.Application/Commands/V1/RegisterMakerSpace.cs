8fe2944eecbf46583ab0a2c8bfff3930a3c8ce0e
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-10-20 17:49:20 +0200

    Functionality to add a MakerSpace implemented

---
+++ src/MakersOfDenmark.Application/Commands/V1/RegisterMakerSpace.cs
@@ -0,0 +1,59 @@
+﻿using MakersOfDenmark.Domain.Enums;
+using MakersOfDenmark.Domain.Models;
+using MakersOfDenmark.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MakersOfDenmark.Application.Commands.V1
+{
+    public class RegisterMakerSpace : IRequest<RegisterMakerSpaceResponse>
+    {
+        public string Name { get; set; }
+        public string AddressStreet { get; set; }
+        public string AddressPostCode { get; set; }
+        public string AddressCountry { get; set; }
+        public string AddressCity { get; set; }
+        public string ContactInfoPhone { get; set; }
+        public string ContactInfoEmail { get; set; }
+        public string VATNumber { get; set; }
+        public string LogoUrl { get; set; }
+        public string AccessType { get; set; }
+    }
+
+    public class RegisterMakerSpaceHandler : IRequestHandler<RegisterMakerSpace, RegisterMakerSpaceResponse>
+    {
+        private readonly MODContext _context;
+
+        public RegisterMakerSpaceHandler(MODContext context)
+        {
+            _context = context;
+        }
+        public async Task<RegisterMakerSpaceResponse> Handle(RegisterMakerSpace request, CancellationToken cancellationToken)
+        {
+            var newAddress = new Address(request.AddressStreet, request.AddressCity, request.AddressCountry, request.AddressPostCode);
+            var newContactInfo = new ContactInfo { Email = request.ContactInfoEmail, Phone = request.ContactInfoPhone };
+            var newMakerSpace = new MakerSpace
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Address = newAddress,
+                ContactInfo = newContactInfo,
+                VATNumber = request.VATNumber,
+                Logo = new Uri(request.LogoUrl),
+                AccessType = (AccessType)Enum.Parse(typeof(AccessType), request.AccessType)
+            };
+            _context.MakerSpace.Add(newMakerSpace);
+            await _context.SaveChangesAsync();
+            return new RegisterMakerSpaceResponse { Id = newMakerSpace.Id };
+        }
+    }
+
+    public class RegisterMakerSpaceResponse
+    {
+        public Guid Id { get; set; }
+    }
+}
--- src/MakersOfDenmark.WebAPI/Controllers/MakerSpaceController.cs
+++ src/MakersOfDenmark.WebAPI/Controllers/MakerSpaceController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MakersOfDenmark.Application.Commands.V1;
 using MakersOfDenmark.Application.Queries.V1;
 using MediatR;
 using Microsoft.AspNetCore.Http;
@@ -24,6 +25,12 @@ namespace MakersOfDenmark.WebAPI.Controllers
         public async Task<IActionResult> GetAll()
         {
             return Ok(await _mediator.Send(new GetAllMakerSpaces()));
+        }
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> RegisterMakerSpace(RegisterMakerSpace request)
+        {
+            return CreatedAtAction("Get", new { id = await _mediator.Send(request) });
         }
     }
 }

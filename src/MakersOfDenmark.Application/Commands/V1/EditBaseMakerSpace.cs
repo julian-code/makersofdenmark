869910b91b5b689f44d2eab51279f1afc50d0acf
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-10-21 09:00:51 +0200

    initial setup

---
+++ src/MakersOfDenmark.Application/Commands/V1/EditBaseMakerSpace.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakersOfDenmark.Application.Commands.V1
+{
+    public class EditBaseMakerSpace : IRequest
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string AddressStreet { get; set; }
+        public string AddressPostCode { get; set; }
+        public string AddressCountry { get; set; }
+        public string AddressCity { get; set; }
+        public string ContactInfoPhone { get; set; }
+        public string ContactInfoEmail { get; set; }
+        public string VATNumber { get; set; }
+        public string LogoUrl { get; set; }
+        public string AccessType { get; set; }
+    }
+}
---
+++ src/MakersOfDenmark.WebAPI/Controllers/AdminMakerSpaceController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MakersOfDenmark.WebAPI.Controllers
+{
+    [ApiController]
+    public class AdminMakerSpaceController : ControllerBase
+    {
+        [HttpPatch("makerspace/{id}")]
+        public async Task<IActionResult> EditMakerSpace()
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPatch("makerspace/{id}/address")]
+        public async Task<IActionResult> EditMakerSpaceAddress()
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPatch("makerspace/{id}/contactinformation")]
+        public async Task<IActionResult> EditMakerSpaceContactInformation()
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPatch("makerspace/{id}/organization")]
+        public async Task<IActionResult> EditMakerSpaceOrganization()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

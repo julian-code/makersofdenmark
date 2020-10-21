7cf52ae3b2ff0d85eb8126d36b41268a98ab2bbb
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-10-21 13:38:11 +0200

    Address endpoint + tests

--- src/MakersOfDenmark.Application/Commands/V1/admin/EditMakerSpaceAddress.cs
+++ src/MakersOfDenmark.Application/Commands/V1/admin/EditMakerSpaceAddress.cs
@@ -1,11 +1,41 @@
-﻿using MediatR;
+﻿using MakersOfDenmark.Domain.Enums;
+using MakersOfDenmark.Domain.Models;
+using MakersOfDenmark.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MakersOfDenmark.Application.Commands.V1.admin
 {
     public class EditMakerSpaceAddress : IRequest
     {
+        public Guid Id { get; set; }
+        public string AddressStreet { get; set; }
+        public string AddressPostCode { get; set; }
+        public string AddressCountry { get; set; }
+        public string AddressCity { get; set; }
+    }
+    public class EditMakerSpaceAddressHandler : IRequestHandler<EditMakerSpaceAddress>
+    {
+        private readonly MODContext _context;
+
+        public EditMakerSpaceAddressHandler(MODContext context)
+        {
+            _context = context;
+        }
+        public async Task<Unit> Handle(EditMakerSpaceAddress request, CancellationToken cancellationToken = default)
+        {
+            var makerSpace = await _context.MakerSpace.Include(x => x.Address).FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (makerSpace == null)
+            {
+                throw new NullReferenceException("Cannot find MakerSpace");
+            }
+            var newAddress = new Address(request.AddressStreet, request.AddressCity, request.AddressCountry, request.AddressCountry);
+            makerSpace.Address = newAddress;
+            await _context.SaveChangesAsync();
+            return new Unit();
+        }
     }
 }

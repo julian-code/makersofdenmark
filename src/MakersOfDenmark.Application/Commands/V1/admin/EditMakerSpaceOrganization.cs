4a560e94d18e3bb8dac949f6d2aea95857373e7f
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-10-21 14:46:55 +0200

    Endpoint for MakerSpaceOrganization + tests

--- src/MakersOfDenmark.Application/Commands/V1/admin/EditMakerSpaceOrganization.cs
+++ src/MakersOfDenmark.Application/Commands/V1/admin/EditMakerSpaceOrganization.cs
@@ -1,10 +1,51 @@
-﻿using System;
+﻿using MakersOfDenmark.Domain.Enums;
+using MakersOfDenmark.Domain.Models;
+using MakersOfDenmark.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MakersOfDenmark.Application.Commands.V1.admin
 {
-    public class EditMakerSpaceOrganization
+    public class EditMakerSpaceOrganization : IRequest
     {
+        public Guid Id { get; set; }
+        public string OrganizationName { get; set; }
+        public string OrganizationStreet { get; set; }
+        public string OrganizationCity { get; set; }
+        public string OrganizationPostCode { get; set; }
+        public string OrganizationCountry { get; set; }
+        public OrganizationType OrganizationType { get; set; }
+    }
+    public class EditMakerSpaceOrganizationHandler : IRequestHandler<EditMakerSpaceOrganization>
+    {
+        private readonly MODContext _context;
+
+        public EditMakerSpaceOrganizationHandler(MODContext context)
+        {
+            _context = context;
+        }
+        public async Task<Unit> Handle(EditMakerSpaceOrganization request, CancellationToken cancellationToken = default) 
+        {
+            var makerSpace = await _context.MakerSpace.Include(x => x.Organization).FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (makerSpace == null)
+            {
+                throw new NullReferenceException("Cannot find MakerSpace");
+            }
+            var newOrgAddress = new Address(request.OrganizationStreet, request.OrganizationCity, request.OrganizationCountry, request.OrganizationPostCode);
+            var newOrganization = new Organization
+            {
+                Address = newOrgAddress,
+                Name = request.OrganizationName,
+                OrganizationType = request.OrganizationType
+            };
+            makerSpace.Organization = newOrganization;
+            await _context.SaveChangesAsync();
+            return new Unit();
+        }
     }
 }
---
+++ tests/MakersOfDenmark.WebAPI.Tests/EditMakerSpaceOrganizationHandlerTest.cs
@@ -0,0 +1,61 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using MakersOfDenmark.Application.Commands.V1.admin;
+using MakersOfDenmark.Domain.Enums;
+using MakersOfDenmark.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MakersOfDenmark.WebAPI.Tests
+{
+    public class EditMakerSpaceOrganizationHandlerTest : IClassFixture<RequestHandlerFixture>
+    {
+        private readonly RequestHandlerFixture _requestHandlerFixture;
+
+        public EditMakerSpaceOrganizationHandlerTest(RequestHandlerFixture requestHandlerFixture)
+        {
+            _requestHandlerFixture = requestHandlerFixture;
+        }
+        [Fact]
+        public async Task EditMakerSpaceOrganizationTest_ValuesAreDifferent()
+        {
+            _requestHandlerFixture.Fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+                .ForEach(b => _requestHandlerFixture.Fixture.Behaviors.Remove(b));
+            _requestHandlerFixture.Fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+
+            var testMakerSpace = _requestHandlerFixture.Fixture.Build<MakerSpace>()
+                .Without(x => x.Id)
+                .Create();
+            _requestHandlerFixture.DbContext.MakerSpace.Add(testMakerSpace);
+            _requestHandlerFixture.DbContext.SaveChanges();
+
+            var request = _requestHandlerFixture.Fixture.Build<EditMakerSpaceOrganization>().With(x => x.Id, testMakerSpace.Id).Create();
+            var handler = new EditMakerSpaceOrganizationHandler(_requestHandlerFixture.DbContext);
+            await handler.Handle(request);
+
+            var postTestMakerSpace = _requestHandlerFixture.DbContext.MakerSpace.Include(x => x.Organization).ThenInclude(x=>x.Address).FirstOrDefault(x => x.Id == testMakerSpace.Id);
+
+            postTestMakerSpace.Organization.Name.Should().Be(request.OrganizationName);
+            postTestMakerSpace.Organization.OrganizationType.Should().Be(request.OrganizationType);
+            postTestMakerSpace.Organization.Address.Street.Should().Be(request.OrganizationStreet);
+            postTestMakerSpace.Organization.Address.City.Should().Be(request.OrganizationCity);
+            postTestMakerSpace.Organization.Address.PostCode.Should().Be(request.OrganizationPostCode);
+            postTestMakerSpace.Organization.Address.Country.Should().Be(request.OrganizationCountry);
+        }
+        [Fact]
+        public async Task EditMakerSpaceContactInfo_ThrowsExceptionWhenMakerSpaceCantBeFound()
+        {
+            var randomId = Guid.NewGuid();
+            var handler = new EditMakerSpaceOrganizationHandler(_requestHandlerFixture.DbContext);
+            var request = _requestHandlerFixture.Fixture.Build<EditMakerSpaceOrganization>().With(x => x.Id, randomId).Create();
+            Func<Task> act = async () => await handler.Handle(request);
+            await act.Should().ThrowAsync<NullReferenceException>();
+        }
+    }
+}

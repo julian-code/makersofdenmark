20605f85a2a4de29e28460cb47de1f01628b6543
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-10-21 16:35:29 +0200

    todo added

--- src/MakersOfDenmark.WebAPI/Controllers/AdminMakerSpaceController.cs
+++ src/MakersOfDenmark.WebAPI/Controllers/AdminMakerSpaceController.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 
 namespace MakersOfDenmark.WebAPI.Controllers
 {
+    // TODO:
+    // Only users with MakerSpace Admin should be able to use these endpoints
     [ApiController]
     public class AdminMakerSpaceController : ControllerBase
     {
@@ -54,15 +56,31 @@ namespace MakersOfDenmark.WebAPI.Controllers
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> AddMakerSpaceTools(AddMakerSpaceTool request)
         {
-            return Ok(await _mediator.Send(request));
+            try
+            {
+                var response = await _mediator.Send(request);
+            } catch (Exception ex)
+            {
+                return NotFound();
+            }
+            
+            return CreatedAtAction(nameof(MakerSpaceController.GetTools), new { Id = request.Id}, request);
         }
         [HttpDelete("MakerSpace/{id}/tools/{toolId}")]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> RemoveMakerSpaceTools(RemoveMakerSpaceTool request)
         {
-            return Ok(await _mediator.Send(request));
+            try
+            {
+                var response = await _mediator.Send(request);
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
+            return Ok();
         }
     }
 }

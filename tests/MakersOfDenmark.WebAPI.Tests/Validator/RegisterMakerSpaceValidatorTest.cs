d75514e246453eb880ba14171675acacb2f49e01
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-10-23 00:00:01 +0200

    One Validation test added for example

---
+++ tests/MakersOfDenmark.WebAPI.Tests/Validator/RegisterMakerSpaceValidatorTest.cs
@@ -0,0 +1,29 @@
+﻿using AutoFixture;
+using FluentValidation.TestHelper;
+using MakersOfDenmark.Application.Commands.V1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MakersOfDenmark.WebAPI.Tests.Validator
+{
+    public class RegisterMakerSpaceValidatorTest : IClassFixture<RequestHandlerFixture>
+    {
+        private readonly RequestHandlerFixture _rhf;
+        private RegisterMakerSpaceValidator _registerMakerSpaceValidator;
+        public RegisterMakerSpaceValidatorTest(RequestHandlerFixture rhf)
+        {
+            _registerMakerSpaceValidator = new RegisterMakerSpaceValidator();
+            _rhf = rhf;
+        }
+
+        [Fact]
+        public void ValidName()
+        {
+            RegisterMakerSpace model = _rhf.Fixture.Build<RegisterMakerSpace>().Without(x =>x.Name).Create();
+            var result = _registerMakerSpaceValidator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+        }
+    }
+}

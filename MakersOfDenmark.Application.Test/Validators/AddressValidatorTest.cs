ed1b4667f25675d80df4c8ad46ac75398e3d7abf
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-10-23 15:26:02 +0200

    tests for some validators

---
+++ MakersOfDenmark.Application.Test/Validators/AddressValidatorTest.cs
@@ -0,0 +1,54 @@
+﻿using AutoFixture;
+using FluentValidation.TestHelper;
+using MakersOfDenmark.Application.Commands.V1;
+using MakersOfDenmark.Application.Commands.Validators;
+using Xunit;
+
+namespace MakersOfDenmark.Application.Tests.Validator
+{
+    public class AddressValidatorTest
+    {
+        private Fixture _fixture;
+        private AddressValidator _validator;
+        public AddressValidatorTest()
+        {
+            _fixture = new Fixture();
+            _validator = new AddressValidator();
+        }
+        [Fact]
+        public void AllFieldsEntered()
+        {
+            var registerMakerSpace = _fixture.Create<RegisterMakerSpace>();
+            var result = _validator.TestValidate(registerMakerSpace);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+        [Fact]
+        public void CountryMissing()
+        {
+            var registerMakerSpace = _fixture.Build<RegisterMakerSpace>().Without(x => x.Country).Create(); ;
+            var result = _validator.TestValidate(registerMakerSpace);
+            result.ShouldHaveValidationErrorFor(x => x.Country);
+        }
+        [Fact]
+        public void CityMissing()
+        {
+            var registerMakerSpace = _fixture.Build<RegisterMakerSpace>().Without(x => x.City).Create(); ;
+            var result = _validator.TestValidate(registerMakerSpace);
+            result.ShouldHaveValidationErrorFor(x => x.City);
+        }
+        [Fact]
+        public void StreetMissing()
+        {
+            var registerMakerSpace = _fixture.Build<RegisterMakerSpace>().Without(x => x.Street).Create(); ;
+            var result = _validator.TestValidate(registerMakerSpace);
+            result.ShouldHaveValidationErrorFor(x => x.Street);
+        }
+        [Fact]
+        public void PostCodeMissing()
+        {
+            var registerMakerSpace = _fixture.Build<RegisterMakerSpace>().Without(x => x.PostCode).Create(); ;
+            var result = _validator.TestValidate(registerMakerSpace);
+            result.ShouldHaveValidationErrorFor(x => x.PostCode);
+        }
+    }
+}
---
+++ MakersOfDenmark.Application.Test/Validators/ValidatorFixture.cs
@@ -0,0 +1,30 @@
+﻿using AutoFixture;
+using MakersOfDenmark.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace MakersOfDenmark.Application.Tests.Validator
+{
+    public class ValidatorFixture : IDisposable
+    {
+        private readonly DbContextOptions<MODContext> _options;
+        public MODContext DbContext { get; private set; }
+        public Fixture Fixture { get; private set; }
+
+        public ValidatorFixture()
+        {
+            _options = new DbContextOptionsBuilder<MODContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            DbContext = new MODContext(_options);
+
+            Fixture = new Fixture();
+        }
+
+        public void Dispose()
+        {
+            DbContext.Dispose();
+        }
+    }
+}

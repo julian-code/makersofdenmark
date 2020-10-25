4221c4c50e361325939df88c8093493ba3d68376
julian-code <email@codeiwrote.net>
2020-10-25 22:05:48 +0100

    added request logging

--- src/MakersOfDenmark.WebAPI/Startup.cs
+++ src/MakersOfDenmark.WebAPI/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace MakersOfDenmark.WebAPI
 {
@@ -60,6 +61,7 @@ namespace MakersOfDenmark.WebAPI
             }
             
             app.UseElmahIoSerilog();
+            app.UseSerilogRequestLogging();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseRouting();

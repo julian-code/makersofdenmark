d00502e20a0bfb86307d4c20fe513e7833f12af0
mnabe <32497085+mnabe@users.noreply.github.com>
2020-10-26 11:42:47 +0100

    Added a UserController

---
+++ src/MakersOfDenmark.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MakersOfDenmark.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+    }
+}

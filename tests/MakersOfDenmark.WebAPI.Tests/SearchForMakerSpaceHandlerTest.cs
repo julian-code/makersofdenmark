d0c5b0433fbfddaf79720fcc14bf687c0a2d9106
mnabe <32497085+mnabe@users.noreply.github.com>
2020-10-26 12:47:20 +0100

    Expanded and refactored Search-for-many-makerspaces test

--- tests/MakersOfDenmark.WebAPI.Tests/SearchForMakerSpaceHandlerTest.cs
+++ tests/MakersOfDenmark.WebAPI.Tests/SearchForMakerSpaceHandlerTest.cs
@@ -4,6 +4,7 @@ using MakersOfDenmark.Application.Queries.V1;
 using MakersOfDenmark.Domain.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -39,12 +40,19 @@ namespace MakersOfDenmark.WebAPI.Tests
         [Fact]
         public async Task SearchForManyMakerSpacesTest()
         {
+            //Configuration
+            _requestHandlerFixture.Fixture.Behaviors.OfType<ThrowingRecursionBehavior>      ().ToList().ForEach(b
+              => _requestHandlerFixture.Fixture.Behaviors.Remove(b));
+             _requestHandlerFixture.Fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
             //Arrange
-            var makerSpaceOne = _requestHandlerFixture.Fixture.Build<MakerSpace>().With(x => x.Name, () => "Aarhus Universitet").With(x => x.Address, new Address("Test Street", "Test City", "Test Country", "Test Postcode")).Without(x => x.ContactInfo).Without(x => x.VATNumber).Without(x => x.Organization).Without(x => x.Tools).Create();
-            var makerSpaceTwo = _requestHandlerFixture.Fixture.Build<MakerSpace>().With(x => x.Name, () => "Aalborg Universitet").With(x => x.Address, new Address("Test Street", "Test City", "Test Country", "Test Postcode")).Without(x => x.ContactInfo).Without(x => x.VATNumber).Without(x => x.Organization).Without(x => x.Tools).Create();
+            var makerSpaceOne = _requestHandlerFixture.Fixture.Build<MakerSpace>().With(x => x.Name, "Aarhus Universitet").Create();
+            var makerSpaceTwo = _requestHandlerFixture.Fixture.Build<MakerSpace>().With(x => x.Name, "Aarhus Universitet").Create();
+            var makerSpaceThree = _requestHandlerFixture.Fixture.Build<MakerSpace>().With(x => x.Name, "Randers Gymnasium").Create();
 
             _requestHandlerFixture.DbContext.MakerSpace.Add(makerSpaceOne);
             _requestHandlerFixture.DbContext.MakerSpace.Add(makerSpaceTwo);
+            _requestHandlerFixture.DbContext.MakerSpace.Add(makerSpaceThree);
             await _requestHandlerFixture.DbContext.SaveChangesAsync();
 
             //Act

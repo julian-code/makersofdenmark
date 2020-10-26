291d45da92114b5c370b300d755d59ec8367ef10
mnabe <32497085+mnabe@users.noreply.github.com>
2020-10-26 11:43:30 +0100

    Register and inject mediatR

--- src/MakersOfDenmark.WebAPI/Controllers/UserController.cs
+++ src/MakersOfDenmark.WebAPI/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +12,11 @@ namespace MakersOfDenmark.WebAPI.Controllers
     [Route("[controller]")]
     public class UserController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
     }
 }

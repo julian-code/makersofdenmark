5bac8c06758f25e13742db01fc49913ca93d5d85
mnabe <32497085+mnabe@users.noreply.github.com>
2020-10-26 10:58:02 +0100

    Added a SearchForManyMakerSpaces-test when the search/list contains multiple responses

--- tests/MakersOfDenmark.WebAPI.Tests/SearchForMakerSpaceHandlerTest.cs
+++ tests/MakersOfDenmark.WebAPI.Tests/SearchForMakerSpaceHandlerTest.cs
@@ -40,8 +40,8 @@ namespace MakersOfDenmark.WebAPI.Tests
         public async Task SearchForManyMakerSpacesTest()
         {
             //Arrange
-            var makerSpaceOne = _requestHandlerFixture.Fixture.Build<MakerSpace>().With(x => x.Name == "Aarhus Universitet").With(x => x.Address, new Address("Test Street", "Test City", "Test Country", "Test Postcode")).Without(x => x.ContactInfo).Without(x => x.VATNumber).Without(x => x.Organization).Without(x => x.Tools).Create();
-            var makerSpaceTwo = _requestHandlerFixture.Fixture.Build<MakerSpace>().With(x => x.Name == "Aalborg Universitet").With(x => x.Address, new Address("Test Street", "Test City", "Test Country", "Test Postcode")).Without(x => x.ContactInfo).Without(x => x.VATNumber).Without(x => x.Organization).Without(x => x.Tools).Create();
+            var makerSpaceOne = _requestHandlerFixture.Fixture.Build<MakerSpace>().With(x => x.Name, () => "Aarhus Universitet").With(x => x.Address, new Address("Test Street", "Test City", "Test Country", "Test Postcode")).Without(x => x.ContactInfo).Without(x => x.VATNumber).Without(x => x.Organization).Without(x => x.Tools).Create();
+            var makerSpaceTwo = _requestHandlerFixture.Fixture.Build<MakerSpace>().With(x => x.Name, () => "Aalborg Universitet").With(x => x.Address, new Address("Test Street", "Test City", "Test Country", "Test Postcode")).Without(x => x.ContactInfo).Without(x => x.VATNumber).Without(x => x.Organization).Without(x => x.Tools).Create();
 
             _requestHandlerFixture.DbContext.MakerSpace.Add(makerSpaceOne);
             await _requestHandlerFixture.DbContext.SaveChangesAsync();

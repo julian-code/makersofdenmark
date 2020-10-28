bd3d5413a433018a9b18d1f9e4837e92a078a08d
Jesper Madsen <jesp209i@edu.ucl.dk>
2020-10-28 18:14:57 +0100

    User setup in domain

---
+++ src/MakersOfDenmark.Domain/Models/User/MODRole.cs
@@ -0,0 +1,16 @@
+﻿using MakersOfDenmark.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakersOfDenmark.Domain.Models.User
+{
+    public class MODRole : Entity<int>
+    {
+        public MODUser User { get; set; }
+        public MakerSpace MakerSpace { get; set; }
+        public MakerSpaceRoles  Role { get; set; }
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+    }
+
+}
---
+++ src/MakersOfDenmark.Domain/Models/User/MODUserFollowMakerSpace.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakersOfDenmark.Domain.Models.User
+{
+    public class MODUserFollowMakerSpace : Entity<int>
+    {
+        public MODUser User { get; set; }
+        public MakerSpace MakerSpace { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+}

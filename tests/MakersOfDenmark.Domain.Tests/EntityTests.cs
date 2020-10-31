04df37a2702f23ab0039e13199e0db0b5d57553a
julian-code <email@codeiwrote.net>
2020-10-31 09:40:30 +0100

    Removed not needed tests

--- tests/MakersOfDenmark.Domain.Tests/EntityTests.cs
+++ tests/MakersOfDenmark.Domain.Tests/EntityTests.cs
@@ -1,5 +1,6 @@
 
 using System;
+using System.Collections.Generic;
 using AutoFixture;
 using FluentAssertions;
 using MakersOfDenmark.Domain.Models;
@@ -13,168 +14,6 @@ namespace MakersOfDenmark.Domain.Tests
         public EntityTests()
         {
             _fixture = new Fixture();
-        }
-
-        [Fact]
-        public void EntityOfInt_Equals_SameIdsDifferentObject_ReturnsTrue()
-        {
-            //Arrange
-            var id = _fixture.Create<int>();
-
-            var testObj1 = _fixture.Build<TestEntityWithInt>().With(x => x.Id, id).Create();
-            var testObj2 = _fixture.Build<TestEntityWithInt>().With(x => x.Id, id).Create();
-
-            //Act
-            var subject = testObj1.Equals(testObj2);
-
-            //Assert
-            subject.Should().BeTrue();
-        }
-
-        [Fact]
-        public void EntityOfInt_Equals_DifferentIdsDifferentObject_ReturnsFalse()
-        {
-            //Arrange
-            var id = _fixture.Create<int>();
-            var notEqualId = id + 1;
-
-            var testObj1 = _fixture.Build<TestEntityWithInt>().With(x => x.Id, id).Create();
-            var testObj2 = _fixture.Build<TestEntityWithInt>().With(x => x.Id, notEqualId).Create();
-
-            //Act
-            var subject = testObj1.Equals(testObj2);
-
-            //Assert
-            subject.Should().BeFalse();
-        }
-
-        [Fact]
-        public void EntityOfInt_Equals_null_ReturnsFalse()
-        {
-            //Arrange
-            var id = _fixture.Create<int>();
-
-            var testObj1 = _fixture.Build<TestEntityWithInt>().With(x => x.Id, id).Create();
-
-            //Act
-            var subject = testObj1.Equals(null);
-
-            //Assert
-            subject.Should().BeFalse();
-        }
-
-        [Fact]
-        public void EntityOfInt_EqualOperatorEquals_SameIdsDifferentObject_ReturnsTrue()
-        {
-            //Arrange
-            var id = _fixture.Create<int>();
-
-            var testObj1 = _fixture.Build<TestEntityWithInt>().With(x => x.Id, id).Create();
-            var testObj2 = _fixture.Build<TestEntityWithInt>().With(x => x.Id, id).Create();
-
-            //Act
-            var subject = testObj1 == testObj2;
-
-            //Assert
-            subject.Should().BeTrue();
-        }
-
-        [Fact]
-        public void EntityOfInt_NotEqualOperatorEquals_DifferentIdsDifferentObject_ReturnsTrue()
-        {
-            //Arrange
-            var id = _fixture.Create<int>();
-
-            var testObj1 = _fixture.Build<TestEntityWithInt>().With(x => x.Id, id).Create();
-            var testObj2 = _fixture.Build<TestEntityWithInt>().With(x => x.Id, id + 1).Create();
-
-            //Act
-            var subject = testObj1 != testObj2;
-
-            //Assert
-            subject.Should().BeTrue();
-        }
-
-        [Fact]
-        public void EEntityOfInt_EqualOperator_null_ReturnsFalse()
-        {
-            //Arrange
-            var id = _fixture.Create<int>();
-
-            var testObj1 = _fixture.Build<TestEntityWithInt>().With(x => x.Id, id).Create();
-            var testObj2 = _fixture.Build<TestEntityWithInt>().With(x => x.Id, id).Create();
-
-            //Act
-            var subject = testObj1 == null;
-
-            //Assert
-            subject.Should().BeFalse();
-        }
-
-        [Fact]
-        public void EntityOfInt_NotEqualOperatorEquals_null_ReturnsTrue()
-        {
-            //Arrange
-            var id = _fixture.Create<int>();
-
-            var testObj1 = _fixture.Build<TestEntityWithInt>().With(x => x.Id, id).Create();
-            var testObj2 = _fixture.Build<TestEntityWithInt>().With(x => x.Id, id + 1).Create();
-
-            //Act
-            var subject = testObj1 != null;
-
-            //Assert
-            subject.Should().BeTrue();
-        }
-
-        [Fact]
-        public void EntityOfInt_NotEqualOperatorEquals_nullReference_ReturnsTrue()
-        {
-            //Arrange
-            var id = _fixture.Create<int>();
-
-            TestEntityWithInt testObj1 = null;
-
-            //Act
-            var subject = testObj1 != null;
-
-            //Assert
-            subject.Should().BeFalse();
-        }
-
-
-        [Fact]
-        public void EntityOfInt_GetHashCode_DifferentIds_ReturnsFalse()
-        {
-            //Arrange
-            var id = _fixture.Create<int>();
-
-            var testObj1 = _fixture.Build<TestEntityWithInt>().With(x => x.Id, id).Create();
-            var testObj2 = _fixture.Build<TestEntityWithInt>().With(x => x.Id, id + 1).Create();
-            var expected = testObj2.GetHashCode();
-
-            //Act
-            var subject = testObj1.GetHashCode();
-
-            //Assert
-            subject.Should().NotBe(expected);
-        }
-
-        [Fact]
-        public void EntityOfInt_GetHashCode_SameIds_ReturnsTrue()
-        {
-            //Arrange
-            var id = _fixture.Create<int>();
-
-            var testObj1 = _fixture.Build<TestEntityWithInt>().With(x => x.Id, id).Create();
-            var testObj2 = _fixture.Build<TestEntityWithInt>().With(x => x.Id, id).Create();
-            var expected = testObj2.GetHashCode();
-
-            //Act
-            var subject = testObj1.GetHashCode();
-
-            //Assert
-            subject.Should().Be(expected);
         }
 
         [Fact]
@@ -322,14 +161,42 @@ namespace MakersOfDenmark.Domain.Tests
             //Assert
             subject.Should().Be(expected);
         }
+
+        [Fact]
+        public void EntityOfInt_NotEqualOperatorEquals_nullReference_ReturnsTrue()
+        {
+            //Arrange
+            TestEntityWithGuid testObj1 = null;
+
+            //Act
+            var subject = testObj1 != null;
+
+            //Assert
+            subject.Should().BeFalse();
+        }
+
+
+        [Fact]
+        public void EntityOfT_HashSet_NoDuplicates() 
+        {
+            //Arrange
+            var id = _fixture.Create<Guid>();
+
+            var testObj1 = _fixture.Build<TestEntityWithGuid>().With(x => x.Id, id).Create();
+            var testObj2 = _fixture.Build<TestEntityWithGuid>().With(x => x.Id, id).Create();
+
+            //Act
+            var subject = new HashSet<TestEntityWithGuid>() {
+                testObj1,
+                testObj2
+            };
+
+            //Assert
+            subject.Should().HaveCount(1);
+        }
     }
 
     internal class TestEntityWithGuid : Entity<Guid>
     {
-    }
-
-    internal class TestEntityWithInt : Entity<int>
-    {
-
     }
 }

e77181cb92e5f81ab2f171adcd9cab5b757a4b36
mnabe <32497085+mnabe@users.noreply.github.com>
2020-12-17 13:25:12 +0100

    Created migrations for badge (and makerspace)

--- src/MakersOfDenmark.Infrastructure/Migrations/20201217122140_MigrationForBadges.Designer.cs
+++ src/MakersOfDenmark.Infrastructure/Migrations/20201217122140_MigrationForBadges.Designer.cs
@@ -4,14 +4,16 @@ using MakersOfDenmark.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MakersOfDenmark.Infrastructure.Migrations
 {
     [DbContext(typeof(MODContext))]
-    partial class MODContextModelSnapshot : ModelSnapshot
+    [Migration("20201217122140_MigrationForBadges")]
+    partial class MigrationForBadges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

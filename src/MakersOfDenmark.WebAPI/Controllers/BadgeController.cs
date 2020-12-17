0bbc79bdbb51b59fba61364eb5a66ba5aa1ec306
mnabe <32497085+mnabe@users.noreply.github.com>
2020-12-17 09:21:08 +0100

    Implemented skeleton for GetAllBadges

---
+++ src/MakersOfDenmark.WebAPI/Controllers/BadgeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MakersOfDenmark.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BadgeController : ControllerBase
+    {
+        private readonly IMediator _mediatR;
+        public BadgeController(IMediator mediatR)
+        {
+            _mediatR = mediatR;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllBadges()
+        {
+            return Ok(_mediatR.Send(new GetAllBadges()));
+        }
+    }
+}
